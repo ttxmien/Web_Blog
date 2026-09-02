a41d7d05ca9904c90565e7d238df4cedd236b246
Mien Tran <xuanmien13dt3@gmail.com>
2026-09-02 17:37:48 +0700

    Update appId for re-Deploy

--- GheBenNgai_Blog/Deploy/sanity.cli.js
+++ GheBenNgai_Blog/Deploy/sanity.cli.js
@@ -5,12 +5,8 @@ export default defineCliConfig({
     projectId: 'gd6oa01a',
     dataset: 'production'
   },
-  // deployment: {
-  //   /**
-  //    * Enable auto-updates for studios.
-  //    * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-  //    */
-  //   autoUpdates: true,
-  //   appId: 'rr44tuvcvd7nfqqkm8fej90',
-  // },
+  deployment: {
+    autoUpdates: true,
+    appId: 'rr44tuvcvd7nfqqkm8fej90',
+  },
 })
