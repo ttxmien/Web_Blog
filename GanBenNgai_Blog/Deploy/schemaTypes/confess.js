eef4ed00c0acb69bb60809f4dd3d7b7e23a12324
Mien Tran <xuanmien13dt3@gmail.com>
2026-09-04 00:24:42 +0700

    test post in Sanity

--- GanBenNgai_Blog/Deploy/schemaTypes/confess.js
+++ GanBenNgai_Blog/Deploy/schemaTypes/confess.js
@@ -1,28 +1,35 @@
-export default {
-  name: 'guiTamTinh', // <-- ĐÂY CHÍNH LÀ SCHEMA TYPE NAME CẦN LẤY
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'guiTamTinh',
   title: 'Gửi Tâm Tình',
   type: 'document',
   fields: [
-    {
+    defineField({
       name: 'title',
       title: 'Tiêu Đề',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       name: 'authorName',
       title: 'Tên Tác Giả / Biệt Danh',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       name: 'authorEmail',
       title: 'Email nhận phản hồi',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime', // <-- Thêm trường thời gian ở đây
+    }),
+    defineField({
       name: 'body',
       title: 'Nội Dung',
       type: 'array',
       of: [{ type: 'block' }]
-    }
+    })
   ]
-}
+})
