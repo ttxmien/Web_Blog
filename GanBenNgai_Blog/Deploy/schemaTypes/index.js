adf98306182fd7ce46aa9d9a4f0707f2e820579c
Mien Tran <xuanmien13dt3@gmail.com>
2026-09-04 00:04:05 +0700

    Update the confess to Sanity

--- GanBenNgai_Blog/Deploy/schemaTypes/index.js
+++ GanBenNgai_Blog/Deploy/schemaTypes/index.js
@@ -2,6 +2,6 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
-import guiTamTinh from './guiTamTinh'
+import guiTamTinh from './confess'
 
 export const schemaTypes = [post, author, category, guiTamTinh, blockContent]
