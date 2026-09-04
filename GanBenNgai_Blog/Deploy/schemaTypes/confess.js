1851857af64914d8fbb851d5a48703a2c9fa8c0a
Mien Tran <xuanmien13dt3@gmail.com>
2026-09-04 15:23:06 +0700

    Update image in confess to Sanity

--- GanBenNgai_Blog/Deploy/schemaTypes/confess.js
+++ GanBenNgai_Blog/Deploy/schemaTypes/confess.js
@@ -29,7 +29,13 @@ export default defineType({
       name: 'body',
       title: 'Nội Dung',
       type: 'array',
-      of: [{ type: 'block' }]
+      of: [
+        { type: 'block' },
+        { 
+          type: 'image',
+          options: { hotspot: true }
+        }
+      ]
     })
   ]
 })
